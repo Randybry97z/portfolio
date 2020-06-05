312da7424b8eabb8682997919d78af219f007524
Bryan <bssandovalrod@gmail.com>
2020-06-05 12:51:48 -0500

    fetch repos to api.github

--- src/components/contact.form.js
+++ src/components/contact.form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => (
 	<form className="mt-16 text-center">
-		<label for="contact-content" className="block text-gray-700 text-sm font-bold mb-2">Cuéntame la idea que tienes en mente: </label>
+		<label htmlFor="contact-content" className="block text-gray-700 text-sm font-bold mb-2">Cuéntame la idea que tienes en mente: </label>
 		<div className="flex shadow rounded bg-white border p-2">
 			<textarea
 				id="contact-content"
--- src/components/repos.js
+++ src/components/repos.js
@@ -1,20 +1,36 @@
-import React from "react"
-import repos from '../data/repos'
+import React, {useEffect, useState} from "react"
+//import repos from '../data/repos'
 import Repo from './repo'
 
 
-export default () => (
-	<div className="max-w-4xl mx-auto">
-		<header className="text-center">
-			<h2 className="text-3xl font-bold">Mi trabajo Open Source</h2>
-			<p>Colaboración y contribución de código</p>
-		</header>
-		<ul className="repos-list">
-			{
-				repos.map((repo)=>{
-					return <Repo repo={repo} key={repo.id} />
-				})
-			}
-		</ul>
-	</div>
-)
+export default () => {
+
+	const [repos,setRepos] = useState([])
+
+	useEffect(async()=>{
+
+		async function fetchRepos() {
+			const response = await fetch("https://api.github.com/users/Randybry97z/repos")
+			let myRepos = await response.json()
+
+			setRepos(myRepos)
+		}
+
+		fetchRepos()
+	},[])
+	return (
+		<div className="max-w-4xl mx-auto">
+			<header className="text-center">
+				<h2 className="text-3xl font-bold">Mi trabajo Open Source</h2>
+				<p>Colaboración y contribución de código</p>
+			</header>
+			<ul className="repos-list">
+				{
+					repos.map((repo)=>{
+						return <Repo repo={repo} key={repo.id} />
+					})
+				}
+			</ul>
+		</div>
+	)
+}
