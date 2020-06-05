f4f14afb61b53de4acca1503608d867dee497055
Bryan <bssandovalrod@gmail.com>
2020-06-05 12:35:51 -0500

    Listing github repos local

--- src/components/contact.form.js
+++ src/components/contact.form.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default () => (
 	<form className="mt-16 text-center">
-		<label for="contact-content" className="block text-gray-700 text-sm font-bold mb-2">Cuéntame que idea tienes en mente: </label>
+		<label for="contact-content" className="block text-gray-700 text-sm font-bold mb-2">Cuéntame la idea que tienes en mente: </label>
 		<div className="flex shadow rounded bg-white border p-2">
 			<textarea
 				id="contact-content"
 				name="contact-content"
 				className="flex-1 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"></textarea>
-			<button>Enviar</button>
+			<button className="btn ml-4">Enviar</button>
 		</div>
 	</form>
 )
