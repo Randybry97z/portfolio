ac1eb95f5a8fe8f2ebb854c751c1e3e1100f9259
Bryan <bssandovalrod@gmail.com>
2020-06-08 13:46:02 -0500

    fixing design responsive

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,13 @@ import Form from './contact.form'
 
 export default ()=>(
 	<header className="bg-gray-300">
-		<div className="container mx-auto p-12 max-w-4xl">
-			<div className="flex justify-center items-center">
+		<div className="container mx-auto p-12 justify-center max-w-4xl">
+			<div className="xl:flex-1 sm:flex justify-center items-center">
 				<div className="flex-1 justify-center">
-					<h1 className="xl: font-bold text-blue-700 text-6xl">¡Hi! I'm Bryan</h1>
+					<h1 className="font-bold text-blue-700 text-6xl">¡Hi! I'm Bryan</h1>
 					<p className="text-xl font-light">I am Software Developer.</p>
 				</div>
-					<img className=" inline-block " src={illustration} alt="Thinking code" style={{height: "300px", width: "400px"}}></img>
+					<img className="inline-block visible" src={illustration} alt="Thinking code" style={{height: "300px", width: "400px"}}></img>
 			</div>
 
 
