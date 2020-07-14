8f7360f3671780586ccc3f489b39a6fb9958581e
Bryan Sandoval <bssandovalrod@gmail.com>
2020-07-14 13:27:11 -0500

    update progress word

--- src/components/course.js
+++ src/components/course.js
@@ -7,7 +7,7 @@ export default (props)=>{
 			<h4 className="font bold">
 				<a href={course.url} target="_blank" rel="noopener noreferrer" className="truncate">{course.title}</a></h4>
 			<div className="text-center">
-				<span className="inline-block bg-blue-200 text-blue-700 p-2 mt-2 radius">Progreso: {course.progress}</span>
+				<span className="inline-block bg-blue-200 text-blue-700 p-2 mt-2 radius">Progress: {course.progress}</span>
 			</div>
 		</div>
 	)
